22e8445cde949f6131cb649b676172437a72d11a
Chukwuemeka Nkwo <emekankwo49@gmail.com>
2023-06-22 16:10:56 +0100

    updated login page and notification handling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,71 +1,72 @@
-import { FormEventHandler, ReactElement, useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { FormEventHandler, ReactElement, useEffect, useState } from 'react';
 import { useLoginMutation } from '../../services/api';
 import { useNavigate } from 'react-router-dom';
-
-import Card from '@components/Card';
-import ErrorMessage from '@components/ErrorMessage';
+import { FilledButton } from '@/shared/UIs/CustomButton';
+import { notification, Spin } from 'antd';
+import { InputField, PassowrdField } from '@/shared/UIs/InputField';
 
 const Home = (): ReactElement => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
-	const [login, loginResult] = useLoginMutation();
+	const [login, { isLoading, isSuccess, error, data }] = useLoginMutation();
 	const navigate = useNavigate();
 
 	const submitForm: FormEventHandler = async (e) => {
 		e.preventDefault();
 
 		try {
-			const { data } = await login({ email, password }).unwrap();
-			localStorage.setItem('authToken', data.token);
-			navigate('/dashboard');
+			await login({ email, password }).unwrap();
+
 		} catch (err) {
 			console.error(err);
 		}
 	};
+	console.log(data);
+
+	useEffect(() => {
+		if (isSuccess) {
+			notification.success({
+				message: "Login Successfully",
+				duration: 3,
+				placement: "topRight",
+			});
+			sessionStorage.setItem('authToken', data?.data.token);
+			setEmail('');
+			setPassword('');
+			navigate('/dashboard');
+		}
+	}, [isSuccess, navigate]);
+
+	useEffect(() => {
+		if (error) {
+			const errorMesg = (error as any).data?.message;
+			notification.error({
+				message: errorMesg,
+				duration: 3,
+				placement: "topRight",
+			});
+		}
+	}, [error]);
 
 	return (
 		<>
-			<div className="container">
-				<Card title="Login">
-					<form onSubmit={submitForm}>
-						<div className="form-group">
-							<label htmlFor="email">E-mail</label>
-							<input
-								type="email"
-								className="form-control"
-								id="email"
-								value={email}
-								onChange={(e) => setEmail(e.target.value)}
-								required
-							/>
-						</div>
-						<div className="form-group">
-							<label htmlFor="password">Password</label>
-							<input
-								type="password"
-								name="password"
-								className="form-control"
-								id="password"
-								value={password}
-								onChange={(e) => setPassword(e.target.value)}
-								required
-							/>
-							<div className="form-group">
-								<button
-									className="btn"
-									type="submit"
-									disabled={loginResult.isLoading}
-								>
-									Submit
-								</button>
-							</div>
-						</div>
-					</form>
-					{loginResult.isLoading && <ErrorMessage message='Loading. Please wait' />}
-					{loginResult.isError && (
-						<ErrorMessage message={(loginResult.error as any).data?.message} />
-					)}
-				</Card>
+			<div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+
+				<form onSubmit={submitForm} className='flex flex-col w-full justify-center gap-3'>
+					<InputField className='w-[350px]' name='email' type='email' title='Email' inputValue={email} onChange={(e) => setEmail(e.target.value)} />
+					<PassowrdField className='w-[350px]' name='password' title='Password' inputValue={password} onChange={(e) => setPassword(e.target.value)} />
+
+					{
+						isLoading ? (
+							<Spin />) : (
+							<FilledButton className='w-full bg-mainColor  text-white' title={isLoading ? "Loading..." : "Login"} />
+
+						)
+					}
+				</form>
+
+
 			</div>
 		</>
 	);
