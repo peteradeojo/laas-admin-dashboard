73011e0c1e4a0f68fef8d78a2d164560a3f1dd9f
Chukwuemeka Nkwo <emekankwo49@gmail.com>
2023-06-22 16:11:35 +0100

    updated redux methodology

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,38 +1,44 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseUrl = import.meta.env.VITE_API_URL;
 
-export const getAuthToken = () => localStorage.getItem('authToken')
-export const setAuthToken = (token: string) => localStorage.setItem('authToken', token)
+// export const getAuthToken = () => sessionStorage.getItem('authToken')
+// export const setAuthToken = (token: string) => localStorage.setItem('authToken', token)
 
 export const authApi = createApi({
-	reducerPath: 'auth',
-	baseQuery: fetchBaseQuery({ baseUrl: baseUrl + 'admin' }),
-	endpoints: (builder) => ({
-		login: builder.mutation({
-			query: (credentials) => ({
-				url: 'auth/login',
-				method: 'POST',
-				body: credentials,
-			}),
-		}),
-		profile: builder.query({
-			query: () => ({
-				url: 'auth',
-				headers: {
-					Authorization: `Bearer ${getAuthToken()}`,
-				}
-			}),
-		}),
-		analytics: builder.query({
-			query: () => ({
-				url: 'analytics',
-				headers: {
-					Authorization: `Bearer ${getAuthToken()}`,
-				}
-			}),
-		})
-	}),
+  reducerPath: "auth",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${baseUrl}/admin`,
+    prepareHeaders: (headers) => {
+      const token = sessionStorage.getItem("authToken");
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  refetchOnReconnect: true,
+  tagTypes: ["Profile", "Analytics"],
+
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: "auth/login",
+        method: "POST",
+        body: credentials,
+      }),
+      invalidatesTags: ["Profile"],
+    }),
+    profile: builder.query({
+      query: () => "auth",
+      providesTags: ["Profile"],
+    }),
+    analytics: builder.query({
+      query: () => "analytics",
+      providesTags: ["Analytics"],
+    }),
+  }),
 });
 
 export const { useLoginMutation, useProfileQuery, useAnalyticsQuery } = authApi;
