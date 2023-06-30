786c6f754920c63a3220d64b906202ca28141bfe
Chukwuemeka Nkwo <emekankwo49@gmail.com>
2023-06-30 15:12:05 +0100

    refactored

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,80 +1,43 @@
-import ErrorMessage from '@/components/ErrorMessage';
+
 import { useGetUsersQuery } from '@/services/userApi';
-// import { useState } from 'react';
-import { Link } from 'react-router-dom';
-
-import styles from './style.module.scss';
-
-export const UserRole = ({ role }: { role?: string }) => {
-	return (
-		<span className={`${styles.roleTag} ${styles[role || 'empty']}`}>
-			{role}
-		</span>
-	);
-};
-
-export const Tag = ({
-	className,
-	tag,
-}: {
-	className?: string;
-	tag: string;
-}) => {
-	return (
-		<span className={`${styles.roleTag} ${styles[className ?? 'user']}`}>{tag}</span>
-	);
-};
+import { UsersComponent } from '@/components';
+import { useEffect, useState } from 'react';
+import { Spin, notification } from 'antd';
 
 const Users = () => {
-	// const [page, setPage] = useState(1);
+	const [userData, setUserData] = useState();
+	const [page] = useState<number>(1);
+	const [count] = useState<number>(20);
 
-	const usersHook = useGetUsersQuery({ page: 1, count: 20 });
+	const { data, isLoading, error } = useGetUsersQuery({ page: page, count: count });
+
+	useEffect(() => {
+		if (error) {
+			const errorMesg = (error) as any
+			notification.error({
+				message: errorMesg.data.message,
+				duration: 3,
+				placement: "topRight",
+			});
+		}
+	}, [error]);
+
+	useEffect(() => {
+		if (data) {
+			setUserData(data?.data);
+		}
+	}, [data])
 
 	return (
-		<>
-			<h1>Users</h1>
+		<div className='flex flex-col gap-2'>
+			<h1 className='text-[20px] font-semibold tracking-wider'>Users</h1>
 
-			<table className={`table ${styles.userList}`}>
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>E-mail</th>
-						<th></th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					{usersHook.isLoading || usersHook.isFetching ? (
-						<tr>
-							<td colSpan={5}>Loading</td>
-						</tr>
-					) : usersHook.isError ? (
-						<tr>
-							<td colSpan={5}>
-								<ErrorMessage message={(usersHook.error as any).message} />
-							</td>
-						</tr>
-					) : (
-						usersHook.data.data.map((user: any) => {
-							return (
-								<tr key={user._id}>
-									<td>{user.name}</td>
-									<td>{user.email}</td>
-									<td>
-										<UserRole role={user.role} />
-									</td>
-									<td>
-										<Link to={user._id} className="btn">
-											View
-										</Link>
-									</td>
-								</tr>
-							);
-						})
-					)}
-				</tbody>
-			</table>
-		</>
+			{
+				isLoading ? (<Spin />) : (
+					<UsersComponent data={userData} count={count} />
+				)
+			}
+		</div>
 	);
 };
 
