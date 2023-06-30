5e5c44b0d10a170da57ff0eb0e5518319a442d9a
Chukwuemeka Nkwo <emekankwo49@gmail.com>
2023-06-30 15:12:51 +0100

    implemented the sidebar data

---
+++ src/shared/constants/index.tsx
@@ -0,0 +1,44 @@
+import { FiLogOut } from "react-icons/fi";
+import { MdDashboard } from "react-icons/md";
+import { IoMdSettings } from "react-icons/io";
+import { FaUserFriends } from "react-icons/fa";
+
+
+export const sidebarItems = [
+    {
+        key: "/dashboard",
+        icon: <MdDashboard />,
+        title: "Dashboard",
+    },
+    // {
+    //   key: "",
+    //   icon: <FaRegEnvelope />,
+    //   title: "Transaction",
+    //   children: [
+    //     {
+    //       key: "/transaction/overview",
+    //       title: "Overview",
+    //     },
+    //     {
+    //       key: "/transaction/refunds",
+    //       title: "Refunds",
+    //     },
+    //   ],
+    // },
+    {
+        key: "/dashboard/users",
+        icon: <FaUserFriends />,
+        title: "Users",
+    },
+    {
+        key: "/dashboard/settings",
+        icon: <IoMdSettings />,
+        title: "Settings",
+    },
+
+    {
+        key: "/login",
+        icon: <FiLogOut />,
+        title: "Log Out",
+    },
+];
